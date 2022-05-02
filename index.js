605db7542b0ca835199075d201b2c2084bec7450
moha5145 <mms.jimma@gmail.com>
2022-05-03 01:06:53 +0200

    update dotenv

--- index.js
+++ index.js
@@ -11,30 +11,30 @@ mongoose.connect(process.env.MONGO_URI);
 const Todo = mongoose.model("Todo", {
     task: String,
     isChecked: Boolean
-  });
+});
 
 app.use(formidable())
 
-app.post('/add-task', async (req, res)=> {
+app.post('/add-task', async (req, res) => {
 
-        const newTodo = new Todo({
-            task: req.fields.task,
-            isChecked: req.fields.isChecked
-        })
-        await newTodo.save(req.fields.tasks)
+    const newTodo = new Todo({
+        task: req.fields.task,
+        isChecked: req.fields.isChecked
+    })
+    await newTodo.save(req.fields.tasks)
     res.json()
 })
 
-app.get('/get-tasks', async (req, res)=> {
+app.get('/get-tasks', async (req, res) => {
     const allTasks = await Todo.find()
 
     res.json(allTasks)
     // console.log(allTasks)
 })
 app.get("*", (req, res) => {
-    res.status(400).json({error: "Page not found"})
+    res.status(400).json({ error: "Page not found" })
 })
-const port = 4000
+const port = process.env.PORT
 app.listen(port, () => {
     console.log(`server listining port: ${port}...`)
 })
